9f6244588be932b4ccc241a24a52dfb480f2a64d
fokox <tanvir1x10t@gmail.com>
2026-08-19 14:51:50 +0600

    set max chars to 600

--- src/components/PageContent.tsx
+++ src/components/PageContent.tsx
@@ -8,7 +8,7 @@ import Underline from "@tiptap/extension-underline";
 import { Color, TextStyle } from "@tiptap/extension-text-style";
 import { useEffect, useRef, useState, useCallback } from "react";
 
-const MAX_CHARS = 500;
+const MAX_CHARS = 600;
 
 function countChars(html: string): number {
   const div = document.createElement("div");
